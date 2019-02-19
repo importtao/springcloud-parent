71079290fe00b70d93cca9cd2ae71638c6fdc385
importtao <import_tao@163.com>
2019-02-19 13:55:41 +0800

    provider

---
+++ demo-provider-user/src/main/java/me/importtao/user/dao/TestDao.java
@@ -0,0 +1,7 @@
+package me.importtao.user.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import me.importtao.user.entity.Test;
+
+public interface TestDao extends BaseMapper<Test> {
+}
